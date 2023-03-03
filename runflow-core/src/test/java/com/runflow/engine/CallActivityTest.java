dab892dd9c97f60ea0d8642258ed8dfb6ad43b52
songhongtu <865681122@qq.com>
2023-03-03 15:42:39 +0800

    增加子流程测试用例

--- runflow-core/src/test/java/com/runflow/engine/CallActivityTest.java
+++ runflow-core/src/test/java/com/runflow/engine/CallActivityTest.java
@@ -1,26 +1,40 @@
 package com.runflow.engine;
 
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 调用活动
  */
 public class CallActivityTest extends BaseTestCase {
 
+    /**
+     * 打印子流程出参
+     * @param map1
+     * @param map2
+     */
+    public void print(Map map1,Map map2) {
+        Set<Map.Entry<Object,Object>> ent = map1.entrySet();
+        for(Map.Entry<Object,Object> entry:ent){
+            if(entry.getValue() instanceof ApplicationTest){
+                logger.info(entry.getKey()+"  :  "+((ApplicationTest)(entry.getValue())).integer);
+            }
+        }
+    }
+
 
     @Test
     public void callActivity() {
+        CallActivityTest callActivityTest = new CallActivityTest();
         Map map = new HashMap();
         ApplicationTest applicationTest = new ApplicationTest();
         map.put("key1", applicationTest);
+        map.put("callActivity",callActivityTest);
         final ExecutionEntityImpl executionEntity = repositoryService.startWorkflow("Process_1", map);
-
-        logger.info("{}", executionEntity.variableInstances);
-
-
     }
 
 
